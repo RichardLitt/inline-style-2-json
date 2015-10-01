e2743b34cd0db45b47bb06733e9a299db97247c2
Richard Littauer <richard.littauer@gmail.com>
2015-10-01 13:30:56 -0400

    Remove lodash

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict'
-var _ = require('lodash')
 
 module.exports = function (str, opts) {
   if (typeof str !== 'string') {
@@ -8,8 +7,7 @@ module.exports = function (str, opts) {
   opts = opts || {}
   var obj = {}
 
-  str = str.split(';')
-  _.each(str, function (string) {
+  str.split(';').forEach(function (string) {
     if (string !== '') {
       var attr = string.split(':')
       obj[attr[0]] = attr[1]
