59965e081c0957788e5d01e5278482329661e7d5
Richard Littauer <richard+github@burntfen.com>
2018-05-17 14:40:53 -0400

    test: add failing test

--- test.js
+++ test.js
@@ -15,3 +15,7 @@ it('should stringify', function () {
 it('should catch colons in urls', function () {
   assert.deepEqual(inlineStyle2Json('position:absolute;background:url(https://mdn.mozillademos.org/files/11305/firefox.png), url(https://mdn.mozillademos.org/files/11307/bubbles.png);'), {'position': 'absolute', 'background': 'url(https://mdn.mozillademos.org/files/11305/firefox.png), url(https://mdn.mozillademos.org/files/11307/bubbles.png)'})
 })
+
+it('should allow nested quotes', () => {
+  assert.deepEqual(inlineStyle2Json("background: url('foo.bmp; display: none;')"), {background: "url('foo.bmp; display: none;')"})
+})
